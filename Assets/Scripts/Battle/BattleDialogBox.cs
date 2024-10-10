3d291628a0165f4e5dd9e08b85134b519be765e0
DESKTOP-8PENHFH\Dell <20211597@eaut.edu.vn>
2024-10-11 06:04:35 +0700

    save

---
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] private int lettersPerSecond;
+    [SerializeField] private Color highlightedColor;
+    
+    [SerializeField] private Text dialogText;
+    [SerializeField] private GameObject actionSelector;
+    [SerializeField] private GameObject moveSelector;
+    [SerializeField] private GameObject moveDetail;
+
+    [SerializeField] private List<Text> actionTexts;
+    [SerializeField] private List<Text> moveTexts;
+
+    [SerializeField] private Text ppText;
+    [SerializeField] private Text typeText;
+    
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+    
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enable)
+    {
+        dialogText.enabled = enable;
+    }
+
+    public void EnableActionSelector(bool enable)
+    {
+        actionSelector.SetActive(enable);
+    }
+
+    public void EnableMoveSelector(bool enable)
+    {
+        moveSelector.SetActive(enable);
+        moveDetail.SetActive(enable);
+    }
+
+    public void UpdateActionSelector(int currentSelector)
+    {
+        for (int i = 0; i < actionTexts.Count; ++i)
+        {
+            if (i == currentSelector)
+                actionTexts[i].color = highlightedColor;
+            else
+                actionTexts[i].color = Color.black;
+        }
+    }
+    public void UpdateMovesSelector(int currentSelector)
+    {
+        for (int i = 0; i < moveTexts.Count; ++i)
+        {
+            if (i == currentSelector)
+                moveTexts[i].color = highlightedColor;
+            else
+                moveTexts[i].color = Color.black;
+        }
+    }
+
+    public void SetNameMove(List<Move> moves)
+    {
+        for()
+    }
+}
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -2,24 +2,93 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+public enum BattleState {Start, PlayerAction, PlayerMove, EnemyMove, Busy}
 public class BattleSystem : MonoBehaviour
 {
+    private BattleState state;
     [SerializeField] private BattleHub enemyHub;
     [SerializeField] private BattleHub playerHub;
     [SerializeField] private BattleUnit enemyUnit;
     [SerializeField] private BattleUnit playerUnit;
+    [SerializeField] private BattleDialogBox dialogBox;
+    private int currentAction;
+    private int currentMove;
 
     private void Start()
     {
-        SetUpBattle();
+        StartCoroutine(SetUpBattle());
     }
-
-    public void SetUpBattle()
+    public IEnumerator SetUpBattle()
     {
         playerUnit.SetUp();
         enemyUnit.SetUp();
         playerHub.SetData(playerUnit.Pokemon);
         enemyHub.SetData(enemyUnit.Pokemon);
+        yield return dialogBox.TypeDialog($"A wild {playerUnit.Pokemon.Base.Name} appeared !");
+        yield return new WaitForSeconds(1f);
+        
+        PlayerAction();
+    }
+
+    public void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action !"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    public void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandheldActionSelector();
+        }
+        else if(state == BattleState.PlayerMove)
+        {
+            HandleMovesSelector();
+        }
+    }
+
+    void HandleMovesSelector()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            currentMove =+ 2;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            currentMove =- 2;
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            currentMove++;
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            currentMove--;
+        currentMove = Mathf.Clamp(currentMove, 0, 3);
+        dialogBox.UpdateMovesSelector(currentMove);
+    }
+    void HandheldActionSelector()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            currentAction++;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            currentAction--;
+        currentAction = Mathf.Clamp(currentAction,0,1);
+        dialogBox.UpdateActionSelector(currentAction);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                // run
+            }
+        }
     }
 }
